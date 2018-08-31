24fd3dc51f8738d876b7e9e5a23670e1ad588a97
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-08-31 21:45:57 +0200

    Make minimal repro

--- src/Core2D.Serializer.Xaml/XmlnsDefinitionsDock.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Portable.Xaml.Markup;
-using Core2D.Serializer.Xaml;
-
-[assembly: XmlnsDefinition(XamlConstants.DockNamespace, "Dock.Model", AssemblyName = "Dock.Model")]
-
-[assembly: XmlnsPrefix(XamlConstants.DockNamespace, "c")]
